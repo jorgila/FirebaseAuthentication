1ae8ad8b1b7202e0bf35fb11b97be8a1d7270c53
Jorge Gimeno <jgimenolatas@gmail.com>
2025-06-24 21:13:55 +0200

    Improving with Clean Architecture

--- app/src/main/java/com/estholon/firebaseauthentication/domain/usecases/authentication/IsPasswordValidUseCase.kt
+++ app/src/main/java/com/estholon/firebaseauthentication/domain/usecases/authentication/IsPasswordValidUseCase.kt
@@ -6,4 +6,12 @@ class IsPasswordValidUseCase @Inject constructor(
 
 ) {
 
+    operator fun invoke(password: String) : Result<Unit> {
+        return when {
+            password.isBlank() -> Result.failure(Exception("La contraseña es requerida"))
+            password.length < 6 -> Result.failure(Exception("La contraseña debe tener al menos 6 caracteres"))
+            else -> Result.success(Unit)
+        }
+    }
+
 }
